4ae7838fb1a4992ebcf9b69f096aacedc6417c8f
mj789-98 <manasjoshi2004.marvel.mj@gmail.com>
2022-12-17 21:00:05 +0530

    new

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 alert("Hello, world!");
+document.getElementById("demo").onmouseover = function() {
+  this.style.backgroundColor = "red";
+}
+document.getElementById("demo").onmouseout = function() {
+  this.style.backgroundColor = "";
+}
 
