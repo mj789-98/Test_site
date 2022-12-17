febdb00420b0e28deece5f413ed651bf1ad14845
mj789-98 <manasjoshi2004.marvel.mj@gmail.com>
2022-12-17 21:16:49 +0530

    bl

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.getElementById("demo").onmouseover = function() {
   this.style.backgroundColor = "blue";
 }
 document.getElementById("demo").onmouseout = function() {
-  this.style.backgroundColor = "";
+  this.style.backgroundColor = "black";
 }
 
