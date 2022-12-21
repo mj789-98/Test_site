b13dd9df9e58b18f3fe675b2ac60f3886bc92438
mj789-98 <manasjoshi2004.marvel.mj@gmail.com>
2022-12-21 08:01:53 +0530

    clock added

--- script.js
+++ script.js
@@ -14,4 +14,37 @@ text.addEventListener('mouseleave', () => {
   text.style.color = 'blue';
 });
 
+function updateClock() {
+  const currentTime = new Date();
+  const currentHours = currentTime.getHours();
+  const currentMinutes = currentTime.getMinutes();
+  const currentSeconds = currentTime.getSeconds();
+ 
+  // Pad the minutes and seconds with leading zeros, if required
+  const currentMinutesLeadingZero = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+  const currentSecondsLeadingZero = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+ 
+  // Choose either "AM" or "PM" as appropriate
+  const timeOfDay = (currentHours < 12) ? "AM" : "PM";
+ 
+  // Convert the hours component to 12-hour format if needed
+  currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+ 
+  // Convert an hours component of "0" to "12"
+  currentHours = (currentHours == 0) ? 12 : currentHours;
+ 
+  // Compose the string for display
+  const currentTimeString = currentHours + ":" + currentMinutesLeadingZero + ":" + currentSecondsLeadingZero + " " + timeOfDay;
+ 
+  // Update the time display
+  document.getElementById("clock").innerHTML = currentTimeString;
+}
+ 
+// When the page loads, set up the clock and start it running
+window.onload = function() {
+  updateClock();
+  setInterval(updateClock, 1000);
+}
+
+
 
