b925c9775ac09d7e0b4df1baa7f6ebe993b54672
mj789-98 <manasjoshi2004.marvel.mj@gmail.com>
2024-06-06 16:50:05 +0530

    assest

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,6 @@ import menu from "./menu.svg";
 
 import algorithms from "./algorithms.png";
 import devnotes from "./devnotes.png";
-import oscs from "./oscs.png";
+import oscs from "./Goku.png";
 
 export { algorithms, close, devnotes, logo, menu, oscs };
-
